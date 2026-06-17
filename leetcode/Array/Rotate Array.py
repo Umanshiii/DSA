46cd8d7829ae51f58fcb0689b36d51b2c0900972
Umanshi <umanshi121@gmail.com>
2026-06-18 01:40:00 +0530

    Coding practice basics

--- leetcode/Array/Rotate Array.py
+++ leetcode/Array/Rotate Array.py
@@ -5,3 +5,31 @@ def rotate(self, nums, k):
     k = k % len(nums)   
     nums[:] = nums[-k:] + nums[:-k]
     return nums
+
+class Solution:
+    def rotateArr(self, arr, d):
+        #code here
+        n=len(arr)
+        d=d%n
+        start=0
+        end=d-1
+        while start<end:
+            arr[start],arr[end]=arr[end],arr[start]
+            start+=1
+            end-=1
+        
+        start=d
+        end=len(arr)-1
+        while start<end:
+            arr[start],arr[end]=arr[end],arr[start]
+            start+=1
+            end-=1
+            
+        start=0
+        end=len(arr)-1
+        while start<end:
+            arr[start],arr[end]=arr[end],arr[start]
+            start+=1
+            end-=1
+        
+        return arr
