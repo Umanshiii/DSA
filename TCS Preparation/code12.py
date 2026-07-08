3bbba64ff63f1daef8319424adca386d7ed9d384
Umanshi <umanshi121@gmail.com>
2026-07-09 00:12:23 +0530

    tcs nqt pyq

--- TCS Preparation/code12.py
+++ TCS Preparation/code12.py
@@ -3,15 +3,18 @@
 def next(arr):
     result=[]
     for left in range(len(arr)):
+        flag=False
         right=left+1
-        while right<len(arr)-1:    
+        while right<len(arr):    
             if arr[left]<arr[right]:
                 result.append(arr[right])
+                flag=True
                 break
             else:
                 right+=1
-        
-    result.append(-1)
+        if not flag:
+            result.append(-1)
+
     return result
 
 print(next([3,4,10,5,6,7]))
