3d140dcd6655755b01eed6732e199bcbacbfab74
Umanshi <umanshi121@gmail.com>
2026-07-11 00:11:53 +0530

    tcs nqt pyq

---
+++ TCS Preparation/code18.py
@@ -0,0 +1,18 @@
+#Rotate Array by K Steps
+'''
+Given an integer array nums and a non-negative integer k, 
+rotate the array to the right by k steps.
+'''
+def rotate(arr, k):
+    n=len(arr)
+    k=k%n
+    def rev(start, end):
+        while start<end:
+            arr[start],arr[end]=arr[end],arr[start]
+            start+=1
+            end-=1
+    rev(0,n-1)
+    rev(0,k)
+    rev(k,n-1)
+
+    return arr
