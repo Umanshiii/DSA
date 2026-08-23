27b3bf9d9f3986ec4bd44afdc68aa015def78bae
Umanshi <umanshi121@gmail.com>
2026-08-23 16:42:37 +0530

    Balanced binary tree

---
+++ leetcode/tree/balanced_bt.py
@@ -0,0 +1,30 @@
+#https://leetcode.com/problems/balanced-binary-tree/description/
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isBalanced(self, root):
+        def depth(node):
+            if not node:
+                return 0
+
+            left=depth(node.left)
+            right=depth(node.right)
+
+            if left==-1 or right==-1:
+                return -1
+
+            if abs(left-right)>1:
+                return -1
+
+            return 1+max(left,right)
+
+        if not root:
+            return True
+
+        return depth(root)!=-1
+        
